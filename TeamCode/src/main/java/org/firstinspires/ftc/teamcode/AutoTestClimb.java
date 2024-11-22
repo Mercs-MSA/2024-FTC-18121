988102de82957db301dd7e00a398318f3351f8a4
mercs 18121 laptop <derek.o@mckinneysteamacademy.org>
2024-11-21 19:31:04 -0600

    Ishaan: added ServoPositioner and AutoTestClimb

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestClimb.java
@@ -0,0 +1,53 @@
+/* Created by Phil Malone. 2023.
+    This class illustrates my simplified Odometry Strategy.
+    It implements basic straight line motions but with heading and drift controls to limit drift.
+    See the readme for a link to a video tutorial explaining the operation and limitations of the code.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/*
+ * This OpMode illustrates an autonomous opmode using simple Odometry
+ * All robot functions are performed by an external "Robot" class that manages all hardware interactions.
+ * Pure Drive or Strafe motions are maintained using two Odometry Wheels.
+ * The IMU gyro is used to stabilize the heading during all motions
+ */
+
+@Autonomous(name="(DON'T USE) Auto Test Climb")
+public class AutoTestClimb extends LinearOpMode
+{
+    // get an instance of the "Robot" class.
+    final private SimplifiedOdometryRobot robot = new SimplifiedOdometryRobot(this);
+
+    @Override public void runOpMode()
+    {
+        // Initialize the robot hardware & Turn on telemetry
+        robot.initialize(true, true);
+
+        // Wait for driver to press start
+        telemetry.addData(">", "Touch Play to run Auto");
+
+        waitForStart();
+
+        robot.resetHeading();  // Reset heading to set a baseline for Auto
+        robot.activate();
+
+        // Run Auto if stop was not pressed.
+        if (opModeIsActive())
+        {
+            for (int i = 0; i < 100; i++) {
+                robot.shoulderWinchRobot(0);
+
+            }
+            for (int i = 0; i < 100; i++) {
+                robot.shoulderAutonClimb(0);
+            }
+            robot.drive(5, 0.60, 0.1);
+        }
+        telemetry.update();
+        robot.incrementOpModeCounter();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPositioner.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="cool")
+public class ServoPositioner extends LinearOpMode {
+    public Servo wrist = null;
+    public void runOpMode() {
+        waitForStart();
+        while (opModeIsActive()) {
+            wrist = hardwareMap.get(Servo.class, "wrist");
+            wrist.setPosition(0.0);
+        }
+    }
+}
